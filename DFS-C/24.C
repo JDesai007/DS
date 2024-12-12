98965857f8002193799ae7b6278746bab303688a
JDesai007 <djaydip889@gmail.com>
2024-12-12 09:49:43 +0530

    Update 24.C

--- DFS-C/24.C
+++ DFS-C/24.C
@@ -1,66 +1,61 @@
-// #include <stdio.h>
-#include <conio.h>
+#include <stdio.h>
 
 void heapify(int arr[], int n, int i) {
-
     int largest = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
 
-    if (l < n && arr[l] > arr[largest]) {
-	largest = l;
+    if (left < n && arr[left] > arr[largest]) {
+        largest = left;
     }
-    if (r < n && arr[r] > arr[largest]) {
-	largest = r;
+
+    if (right < n && arr[right] > arr[largest]) {
+        largest = right;
     }
 
     if (largest != i) {
-	int temp = arr[i];
-	arr[i] = arr[largest];
-	arr[largest] = temp;
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
 
-	heapify(arr, n, largest);
+        heapify(arr, n, largest);
     }
 }
 
 void heapSort(int arr[], int n) {
-
-    int i;
-    for (i = n / 2 - 1; i >= 0; i--) {
-	heapify(arr, n, i);
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        heapify(arr, n, i);
     }
 
-    for (i = n - 1; i > 0; i--) {
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
 
-	int temp = arr[0];
-	arr[0] = arr[i];
-	arr[i] = temp;
-
-	heapify(arr, i, 0);
+        heapify(arr, i, 0);
     }
 }
 
-void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++) {
-	printf("%d ", arr[i]);
-    }
-    printf("\n");
-}
+int main() {
+    int n;
 
-void main() {
-    int arr[] = {9, 4, 3, 8, 10, 2, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int i;
-    clrscr();
-    printf("Array in list:\n");
-    for(i=0;i<n;i++)
-    {
-	printf("%d ",arr[i]);
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
+
     heapSort(arr, n);
 
-    printf("\n\nSorted array is \n");
-    printArray(arr, n);
-    getch();
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
 }

89d11dcb3cdcf0cd264a2f06a33bbf60dd2bae3a
JDesai007 <djaydip889@gmail.com>
2024-12-12 09:48:43 +0530

    Update 23.C

--- DFS-C/23.C
+++ DFS-C/23.C
@@ -1,13 +1,8 @@
 #include <stdio.h>
-#include <conio.h>
-#include <stdlib.h>
-
-#define MAX 10
 
 int getMax(int arr[], int n) {
     int max = arr[0];
-    int i;
-    for (i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         if (arr[i] > max) {
             max = arr[i];
         }
@@ -15,59 +10,56 @@ int getMax(int arr[], int n) {
     return max;
 }
 
-void countingSort(int arr[], int n, int exp) {
-    int *output = (int *)malloc(n * sizeof(int));
+void countSort(int arr[], int n, int exp) {
+    int output[n];
     int count[10] = {0};
-    int i;
 
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         count[(arr[i] / exp) % 10]++;
     }
 
-    for (i=1; i < 10; i++) {
+    for (int i = 1; i < 10; i++) {
         count[i] += count[i - 1];
     }
 
-    for (i = n - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--) {
         output[count[(arr[i] / exp) % 10] - 1] = arr[i];
         count[(arr[i] / exp) % 10]--;
     }
 
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         arr[i] = output[i];
     }
-
-    free(output);  // Free dynamically allocated memory
 }
 
 void radixSort(int arr[], int n) {
     int max = getMax(arr, n);
-    int exp;
-    for (exp = 1; max / exp > 0; exp *= 10) {
-        countingSort(arr, n, exp);
+
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+        countSort(arr, n, exp);
     }
 }
 
-void printArray(int arr[], int n) {
-    int i;
-    for (i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+int main() {
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements: \n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    printf("\n");
-}
-
-void main() {
-    int arr[] = {170, 45, 75, 90, 802, 24, 2, 66};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    clrscr();
-    printf("Original Array: \n");
-    printArray(arr, n);
 
     radixSort(arr, n);
 
-    printf("\nSorted Array: \n");
-    printArray(arr, n);
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
 
-    getch();
+    printf("\n");
+    return 0;
 }
